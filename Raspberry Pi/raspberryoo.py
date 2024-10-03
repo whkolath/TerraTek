a0562bc246fb8feefb9f277dc6e7144ae7051be5
whkolath <149017592+whkolath@users.noreply.github.com>
2024-10-03 13:54:25 -0500

    data hiding

--- Raspberry Pi/raspberryoo.py
+++ Raspberry Pi/raspberryoo.py
@@ -10,109 +10,133 @@ thread_lock = threading.Lock()
 
 class Config:
     def __init__(self, config_file):
-        self.config_file = config_file
-        self.config = configparser.ConfigParser()
+        self.__config_file = config_file
+        self.__config = configparser.ConfigParser()
 
     def read_database_config(self):
 
-        self.config.read(self.config_file)
-        host = self.config.get('Database', 'host')
-        user = self.config.get('Database', 'user')
-        password = self.config.get('Database', 'password')
-        database = self.config.get('Database', 'database')
+        self.__config.read(self.__config_file)
+        host = self.__config.get('Database', 'host')
+        user = self.__config.get('Database', 'user')
+        password = self.__config.get('Database', 'password')
+        database = self.__config.get('Database', 'database')
 
         return host, user, password, database
 
 
 class Database:
     def __init__(self, host, user, password, database_name):
-        self.host = host
-        self.user = user
-        self.password = password
-        self.database_name = database_name
+        self.__host = host
+        self.__user = user
+        self.__password = password
+        self.__database_name = database_name
+        self.__connection = None
+        self.__cursor = None
 
+    def is_connected(self):
+        if self.__connection == None:
+            return False
+        else:
+            return True
+    
     def connect(self):
-        return pymysql.connect(
-            host=self.host,
-            user=self.user,
-            password=self.password,
-            database=self.database_name
-        )
+        if self.is_connected() == False:
+            try:
+                self.__connection = pymysql.connect(
+                    host=self.__host,
+                    user=self.__user,
+                    password=self.__password,
+                    database=self.__database_name
+                )
+                self.__cursor = self.__connection.cursor()
+                return True
+            except Exception as e:
+                print(str(e))
+                return False
+
+    def close_connection(self):
+        if self.is_connected() == True:
+            self.__cursor.close()
+            self.__connection.close()
+            self.__cursor = None
+            self.__connection = None
 
     def insert_into_readings(self, Board_ID, Sensor_ID, Sensor_Value, Sensor_Timestamp):
-        connection = None
-        cursor = None
-        try:
-            connection = self.connect()
-            cursor = connection.cursor()
-            cursor.execute("INSERT INTO Readings (Board_ID, Sensor_ID, Sensor_Value, Sensor_Timestamp) VALUES (%s, %s, %s, %s)",
-                           (Board_ID, Sensor_ID, Sensor_Value, Sensor_Timestamp))
-            connection.commit()
+        if self.is_connected() == True:
+            try:
+                self.__cursor.execute("INSERT INTO Readings (Board_ID, Sensor_ID, Sensor_Value, Sensor_Timestamp) VALUES (%s, %s, %s, %s)",
+                            (Board_ID, Sensor_ID, Sensor_Value, Sensor_Timestamp))
+                self.__connection.commit()
 
-        except Exception as e:
-            print(str(e))
+            except Exception as e:
+                print(str(e))
 
-        finally:
-            if cursor:
-                cursor.close()
-            if connection:
-                connection.close()
 
 
 class Board:
     def __init__(self, hostname, sensors):
-        self.hostname = hostname
-        self.sensors = sensors
-        self.network = Network_Connection(self.hostname)
+        self.__hostname = hostname
+        self.__sensors = sensors
+        self.__network = Network_Connection(self.__hostname)
 
+    def get_hostname(self):
+        return self.__hostname
+    
+    def get_sensors(self):
+        return self.__sensors
+    
     def get_readings(self):
-        return self.network.get_readings()
+        return self.__network.get_readings()
 
 
 class Data_Collector:
     def __init__(self, board, database):
-        self.board = board
-        self.database = database
+        self.__board = board
+        self.__database = database
 
     def read_all(self):
-        r = self.board.get_readings()
+        r = self.__board.get_readings()
         if r != None:
             timestamp = str(datetime.now())
-            for sensor in self.board.sensors:
-                self.database.insert_into_readings(
-                    self.board.hostname, sensor.sensor_name, sensor.read(r), timestamp)
+            self.__database.connect()
+            for sensor in self.__board.get_sensors():
+                self.__database.insert_into_readings(
+                    self.__board.get_hostname(), sensor.get_sensor_name(), sensor.read(r), timestamp)
+            self.__database.close_connection()
 
     def read_sensor(self, sensor_name):
-        r = self.board.get_readings()
+        r = self.__board.get_readings()
         if r != None:
-            for sensor in self.board.sensors:
-                if (sensor.sensor_name == sensor_name):
-                    self.database.insert_into_readings(
-                        self.board.hostname, sensor.sensor_name, sensor.read(r), str(datetime.now()))
+            for sensor in self.__board.get_sensors():
+                if (sensor.get_sensor_name() == sensor_name):
+                    self.__database.connect()
+                    self.__database.insert_into_readings(
+                        self.__board.get_hostname(), sensor.get_sensor_name(), sensor.read(r), str(datetime.now()))
+                    self.__database.close_connection()
 
 
 class Data_Collector_Thread(threading.Thread):
     def __init__(self, data_collector, delay=1800):
         threading.Thread.__init__(self)
-        self.data_collector = data_collector
-        self.delay = delay
+        self.__data_collector = data_collector
+        self.__delay = delay
         self.daemon = True
 
     def run(self):
         while True:
             thread_lock.acquire()
-            self.data_collector.read_all()
+            self.__data_collector.read_all()
             thread_lock.release()
-            sleep(self.delay)
+            sleep(self.__delay)
 
 
 class Network_Connection:
     def __init__(self, hostname):
-        self.hostname = hostname
+        self.__hostname = hostname
 
     def get_readings(self):
         try:
-            r = requests.get(f"http://{self.hostname}.local", timeout=5)
+            r = requests.get(f"http://{self.__hostname}.local", timeout=5)
             r.raise_for_status()
             return r.json()
 
@@ -123,8 +147,11 @@ class Network_Connection:
 
 class Sensor:
     def __init__(self, sensor_name):
-        self.sensor_name = sensor_name
+        self.__sensor_name = sensor_name
 
+    def get_sensor_name(self):
+        return self.__sensor_name
+    
     def read(self):
         raise NotImplementedError(
             "This method should be overridden in child classes")
