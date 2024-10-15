ee90650516046e17349cbc227f512476abd96bf5
whkolath <149017592+whkolath@users.noreply.github.com>
2024-10-15 09:55:59 -0500

    Updated Database

--- Database/TerraTek.sql
+++ Database/TerraTek.sql
@@ -37,13 +37,80 @@ INSERT INTO Boards  (Board_ID, Location)
 VALUES  ("testingboard1", "WTAMU Campus" );
 
 INSERT INTO Boards  (Board_ID, Location)
-VALUES  ("rwh1", "RWH Tank on East Side of House" );
+VALUES  ("rwh1", "1250 gallon fresh water RWH tank on north side of property." );
+
+INSERT INTO Boards  (Board_ID, Location)
+VALUES  ("rwh2", "1250 gallon fresh water RWH tank on west side of property." );
+
+INSERT INTO Boards  (Board_ID, Location)
+VALUES  ("rwh3", "3750 gallon fresh water RWH tank on east side of property." );
+
+INSERT INTO Boards  (Board_ID, Location)
+VALUES  ("rwh4", "3750 gallon grey water RWH tank on south side of property." );
+
+INSERT INTO Boards  (Board_ID, Location)
+VALUES  ("Weather_Station", "Garden" );
+
+
+-- env_iii
 
 INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
 VALUES  ("SHT31_Temperature", "Atmospheric Temperature", "Celsius" );
 
 INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
 VALUES  ("SHT31_Humidity", "Humidity", "Percent" );
+
+
+-- MKR_Environmental_Shield
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("MKR_Environmental_Shield_Temperature", "Atmospheric Temperature", "Celsius" );
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("MKR_Environmental_Shield_Humidity", "Humidity", "Percent" );
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("MKR_Environmental_Shield_Pressure", "Barometric Pressure", "Kilopascals" );
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("MKR_Environmental_Shield_Illuminance", "Illuminance", "lux" );
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("MKR_Environmental_Shield_UVA", "Ultra Violet A", "?" );
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("MKR_Environmental_Shield_UVB", "Ultra Violet B", "?" );
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("MKR_Environmental_Shield_UvIndex", "UV Index", "?" );
+
+
+
+-- SparkFun_Weather_Meter_Kit
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("SEN-15901_Weather_Meter_Wind_Speed", "Wind Speed", "Kilometers per Hour" );
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("SEN-15901_Weather_Meter_Wind_Direction", "Wind Direction", "Degrees" );
+
+
+-- DFRobot Gravity Analog Electrical Conductivity Meter
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("DFR0300_Conductivity_Meter", "Electrical Conductivity", "ms/cm" );
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("DFR0300_Temperature", "Water Temperature", "Celsius" );
+
+
+-- Waterproof Ultrasonic Distance Sensor
+
+INSERT INTO Sensors  (Sensor_ID, Sensor_Type, Units)
+VALUES  ("ME007YS_Ultrasonic_Distance", "Tank Level Sensor", "CM" );
+
+
+
 
 INSERT INTO Readings  (Board_ID, Sensor_ID, Sensor_Value)
 VALUES  ("testingboard1", "SHT31_Temperature", 23.562);
